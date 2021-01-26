e564975585a4fea33c9f4ca9485bc9f181f6574d
Waylon Dixon <wayjay4@yahoo.com>
2021-01-25 23:32:38 -0700

    cleaned up seeder files

--- database/seeders/JobtypeSeeder.php
+++ database/seeders/JobtypeSeeder.php
@@ -9,46 +9,46 @@ use App\Models\Jobtype;
 
 class JobtypeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-      $jobTypes = array(
-        'core' => [1,2,3],
-        'data hall a' => [4,5,6],
-        'data hall b' => [7,8,9],
-        'data hall c' => [10,11,12],
-        'data hall d' => [13,14,15],
-        'equipment and rentals' => [16,17],
-        'freight' => [18],
-        'general systems' => [19,20,21],
-        'indirect staffing' => [22],
-        'non-scheduled non productive time' => [29],
-        'safety meetings and warm-up' => [23],
-        'scheduled non productive time' => [24],
-        'training' => [25,26],
-        'travel' => [27,28],
-        'warehousing' => [30],
-      );
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    $jobTypes = array(
+      'core' =>                               [1,2,3],
+      'data hall a' =>                        [4,5,6],
+      'data hall b' =>                        [7,8,9],
+      'data hall c' =>                        [10,11,12],
+      'data hall d' =>                        [13,14,15],
+      'equipment and rentals' =>              [16,17],
+      'freight' =>                            [18],
+      'general systems' =>                    [19,20,21],
+      'indirect staffing' =>                  [22],
+      'non-scheduled non productive time' =>  [29],
+      'safety meetings and warm-up' =>        [23],
+      'scheduled non productive time' =>      [24],
+      'training' =>                           [25,26],
+      'travel' =>                             [27,28],
+      'warehousing' =>                        [30],
+    );
       
-      foreach($jobTypes as $key=>$value){
-        $jobtypeID = DB::table('jobtypes')->insertGetId([
-          'name' => $key,
-          'description' => ''
-        ]);
+    foreach($jobTypes as $key=>$value){
+      $jobtypeID = DB::table('jobtypes')->insertGetId([
+        'name' => $key,
+        'description' => ''
+      ]);
 
-        $jobtype = Jobtype::findOrFail($jobtypeID);
+      $jobtype = Jobtype::findOrFail($jobtypeID);
 
-        // $subjobs = Subjob::all()
-        //       ->random(mt_rand(1,4))
-        //       ->pluck('id');
+      // $subjobs = Subjob::all()
+      //       ->random(mt_rand(1,4))
+      //       ->pluck('id');
 
-        $subjobs = $value;
+      $subjobs = $value;
 
-        $jobtype->subjobs()->attach($subjobs);
-      }
+      $jobtype->subjobs()->attach($subjobs);
     }
+  }
 }
