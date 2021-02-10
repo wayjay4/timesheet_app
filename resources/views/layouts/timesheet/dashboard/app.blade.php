58a07b9d1975c23e57795cd2745fbaf209c7030b
Waylon Dixon <wayjay4@yahoo.com>
2021-02-10 14:50:09 -0700

    added t-padding to sidebar and made the links one line instead of two

--- resources/views/layouts/timesheet/dashboard/app.blade.php
+++ resources/views/layouts/timesheet/dashboard/app.blade.php
@@ -51,37 +51,37 @@
                             <div class="sidebar-sticky pt-12">
                                 <ul class="nav flex-column">
                                     <li class="nav-item">
-                                        <a class="nav-link active" href="#">
+                                        <a class="nav-link d-flex active" href="{{ route('dashboard') }}">
                                             <span data-feather="home"></span>
                                             Timesheets <span class="sr-only">(current)</span>
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="file"></span>
                                             Orders
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="shopping-cart"></span>
                                             Products
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="users"></span>
                                             Customers
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="bar-chart-2"></span>
                                             Reports
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="layers"></span>
                                             Integrations
                                         </a>
@@ -96,25 +96,25 @@
                                 </h6>
                                 <ul class="nav flex-column mb-2">
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="file-text"></span>
                                             Current month
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="file-text"></span>
                                             Last quarter
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="file-text"></span>
                                             Social engagement
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#">
+                                        <a class="nav-link d-flex" href="#">
                                             <span data-feather="file-text"></span>
                                             Year-end sale
                                         </a>
